799b2ead4edc7310555a570f5e0bd184abdf3a02
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-06-14 22:44:20 -0300

    Função Parte 1 - Descrevendo como está funcionando o programa de função Pura

--- Python/Python_Outros_paradigmas/1Linguagem_funcional/funcao1.py
+++ Python/Python_Outros_paradigmas/1Linguagem_funcional/funcao1.py
@@ -2,13 +2,25 @@
 valor = 20
 
 def multiplica(fator):
+    # referenciando a variável global
     global valor
-    valor *= fator
+    valor *= fator # Multiplicando o valor da variável global pelo valor do fator
     print(f"Resultado: {valor}")
     
 def main():
     numero = int(input("Digite um número: "))
-    multiplica(numero)
-    multiplica(numero)
+    multiplica(numero) # valor da variável global * número digitado
+    multiplica(numero) # resultado anterior * valor digitado
     
 main()
+
+"""
+Neste caso
+    - O valor da variável global inicia 20
+    - Após solicitar um número para o usuário: Exemplo, o número 3
+    - e chamar a função main:
+        - No primeiro multiplicar:
+            - O valor da variável global será 20 * 3 = 60
+        - Na segunda vez:
+            - O valor da variável global será 60 * 3 = 180
+"""
