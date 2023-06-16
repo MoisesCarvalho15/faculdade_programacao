152d4b173add39d1f776a79b15033412eaae6989
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-06-15 22:47:35 -0300

    Revert "Utilizando a"
    This reverts commit 54db6cfa04bfa74f2fb060b624c51819b40c4ac1.

--- Python/Python_Outros_paradigmas/1Linguagem_funcional/funcao_map_lambda.py
+++
@@ -1,7 +0,0 @@
-numeros = [1, 2, 3, 4, 5]
-
-nova_lista = map(lambda lista_numeros: lista_numeros * 3, numeros)
-nova_lista = list(nova_lista)
-
-print(f"Lista Original: {numeros}")
-print(f"Lista do Resultado: {nova_lista}")
