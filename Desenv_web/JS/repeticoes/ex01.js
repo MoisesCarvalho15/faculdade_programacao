9987b4972181ba8daa0fd48612cba6e51cb1f630
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-06-17 13:51:31 -0300

    Utilizando a função for

---
+++ Desenv_web/JS/repeticoes/ex01.js
@@ -0,0 +1,14 @@
+// Criando uma variável vazia
+var contador;
+
+// Incrementando o valor da variável de 1 em 1 até o número 9
+for (contador = 0; contador < 10; contador ++) {
+    console.log(contador);    
+}
+
+console.log('')
+
+// Incrementando do 1 até o número 5
+for (contador = 1; contador <= 5; contador ++) {
+    console.log(contador)
+}
