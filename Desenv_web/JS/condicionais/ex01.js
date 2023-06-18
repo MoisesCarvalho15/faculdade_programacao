3b365ee5847d84d21f87d2215ad818a426b6f65d
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-06-18 11:35:18 -0300

    Ex01 - If com uma condição

---
+++ Desenv_web/JS/condicionais/ex01.js
@@ -0,0 +1,22 @@
+var a = 10;
+var b = 3;
+
+console.log('If com uma única condição')
+console.log('Valor da letra A: ' + a)
+console.log('Valor da letra B: ' + b)
+
+if (a > b){
+    console.log('"A" é MAIOR que "B"')
+}
+
+if (a == b){
+    console.log('"A" é IGUAL a "B"')
+}
+
+if (a < b){
+    console.log('"A"  é MENOR que "B"')
+}
+
+if (b < a) {
+    console.log('"B" é MENOR que "A"')
+}
