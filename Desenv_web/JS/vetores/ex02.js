6060be9a0722e2ed4101e7f80f24db7c8858d089
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-06-18 12:05:47 -0300

    Ex02 - Utilizando o método push e length

---
+++ Desenv_web/JS/vetores/ex02.js
@@ -0,0 +1,9 @@
+var nomes = [];
+
+// adicionando elementos através do push
+nomes.push('Moisés', 'Carlos', 'Patrícia');
+
+console.log(nomes);
+
+// Verificando a quantidade de elementos de um array
+console.log('Quantidade de nomes na variável: ' + nomes.length);
