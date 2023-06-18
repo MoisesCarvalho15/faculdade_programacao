8939573099ad05c83aebd959756d3e5e33da96e5
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-06-18 12:00:51 -0300

    Ex 01 - Trabalhando com arrays

---
+++ Desenv_web/JS/vetores/ex01.js
@@ -0,0 +1,13 @@
+// criando um array vazio
+var alunos = []
+
+// adicionando elementos no array
+var alunos = new Array('Alex', 'Ana', 'João')
+
+// acessando um array através de seu índice
+console.log(alunos[0])
+
+// Imprimindo todos os elementos do array
+for (aluno in alunos){
+    console.log(alunos[aluno])
+}
